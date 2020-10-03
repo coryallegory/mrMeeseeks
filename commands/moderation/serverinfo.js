ca51111c3401e99339c6568e6df420dc4ee360c7
Ricardo Godoy <rq.godoy@gmail.com>
2020-10-03 09:44:52 -0300

    fix serverinfo command

--- commands/moderation/serverinfo.js
+++ commands/moderation/serverinfo.js
@@ -12,16 +12,18 @@ module.exports.run = async (bot, message, args) =>
 
     let guild = message.channel.guild;
 
-    let userCount = guild.memberCount;
+    let userCount = guild.members.filter((user) => !user.user.bot).size;
+    let botCount = guild.members.filter((user) => user.user.bot).size;
     let name = guild.name;
     let owner = guild.owner;
-    let roles = guild.roles.size;
+    let roles = guild.roles.filter((role) => role.name !== '@everyone').size;
 
     const embed = new Discord.RichEmbed()
     .setTitle(`Server information`)
     .setTimestamp(now)
     .setColor(colors.orange)
     .addField(`Usercount:`, `The usercount is: ${userCount}`)
+    .addField(`Botcount:`, `The botcount is: ${botCount}`)
     .addField(`Guild name:`, `The guild name is: ${name}`)
     .addField(`Guild owner`, `The guild owner is: ${owner}`)
     .addField(`Roles`, `There are ${roles} roles in this server`);
